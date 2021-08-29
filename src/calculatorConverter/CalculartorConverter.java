dc8c2b1bc0c2481d4b396ede0b51375b46105b54
GDavidH <49444102+GDavidH@users.noreply.github.com>
2021-08-28 21:38:05 -0600

    Add Coverter base 10 to X

--- src/calculatorConverter/CalculartorConverter.java
+++ src/calculatorConverter/CalculartorConverter.java
@@ -1,5 +1,7 @@
 package calculatorConverter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class CalculartorConverter {
@@ -66,6 +68,98 @@ public class CalculartorConverter {
 		};
 		
 		return value;
+	}
+	
+	
+	
+	//Cambio de base 10 a base X-------------------------------------
+	private String convierteParteEntera(String parteEntera,int baseDestino) {
+		String result = "";
+		int numToConvert = Integer.parseInt(parteEntera);
+        List<Integer> intArray = new ArrayList<Integer>();
+		
+        //division to get new base
+		while(numToConvert >= baseDestino ) {
+			intArray.add(numToConvert % baseDestino);
+			numToConvert = numToConvert/baseDestino;
+			if(numToConvert < baseDestino) {
+				intArray.add(numToConvert);
+			}
+		}
+		//invert arrayList 
+		for (int i = (intArray.size()-1); i >= 0 ; i--) {
+			//the value is hexadecimal
+			if(intArray.get(i)>9) {
+				result += hexadecimalValue(intArray.get(i));
+			}else
+				result += intArray.get(i);		
+		}	
+		return result;	
+	}
+	
+	
+	private String convierteParteFraccion(String parteFraccion,int baseDestino, int numDigitos) {
+		String result = "";
+		String decimalsNum="";
+		String numToConvert =  "";
+		String exponent = "";
+		double div = Double.parseDouble(parteFraccion);
+		int count = 1;
+		
+		//multiplication to get the new decimal base
+		while ( count <= numDigitos  &&  !decimalsNum.equals("0")) {		
+			numToConvert = "" + (div*baseDestino);			
+			String[] parts = numToConvert.split(Pattern.quote("."));
+			exponent = parts[0];
+			decimalsNum = parts[1];
+			
+			//the value is hexadecimal
+			int intExponent = Integer.parseInt(exponent);
+			if(intExponent>9) {
+				result += hexadecimalValue(intExponent);
+			}else
+				result+=exponent;			
+			div = Double.parseDouble("0."+decimalsNum);
+			count++;
+		}	
+		return result;	
+	}
+	
+	public String convierteDeBaseDiez(String numero,int baseDestino,int numDigitos) {
+		String result = "";
+		String[] parts = numero.split(Pattern.quote("."));
+		
+		//has decimals
+		if(parts.length>1) {
+			String exponent = parts[0];
+			String decimalsNum = "0."+parts[1];
+			result += convierteParteEntera(exponent, baseDestino);
+			result += ".";
+			result += convierteParteFraccion(decimalsNum, baseDestino, numDigitos);
+		}else
+			result += convierteParteEntera(numero, baseDestino);
+		return result;	
+	}
+	
+	//method hexadecimal value
+	private String hexadecimalValue(int num) {
+		String result = "";
+		switch (num) {
+		case 10: result += "A";
+			break;
+		case 11: result += "B";
+			break;
+		case 12:  result += "C";
+			break;
+		case 13: result += "D";
+			break;
+		case 14: result += "E";
+			break;
+		case 15: result += "F";
+			break;
+		};	
+		
+		return result;
 	}
 	
 }
